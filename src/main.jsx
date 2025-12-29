82ebe1c8d3ed5f451ac47999afdfa77f609ead3f
aaronpaladin <aaron.paladinspy@gmail.com>
2025-12-28 18:24:13 -0600

    main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MultiAIOrchestrator from './MultiAIOrchestrator.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MultiAIOrchestrator />
+  </React.StrictMode>
+)
